2445ba23fbe0541853b082b24692f36c880b340b
Treasure-B <thabiso.magagula@gmail.com>
2023-08-11 12:46:38 -0700

    all updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-  """Prints the sum of 1 and 2"""
-# imports the function def add(a, b): from the file add_0.py
-
-from add_0 import add
-
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+    """Sum of 1 and 2"""
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum, divide, multiple and subtraction of 10 and 5."""
+    from calculator_1 import add, subtract, multiply, divide
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, subtract(a, b)))
+    print("{} * {} = {}".format(a, b, multiply(a, b)))
+    print("{} / {} = {}".format(a, b, divide(a, b)))
+
