a64df24d646e5269b17eebfc1ab5a1a4e484a876
Treasure-B <thabiso.magagula@gmail.com>
2023-08-11 14:15:20 -0700

    1-calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the sum, divide, multiple and subtraction of 10 and 5."""
-    from calculator_1 import add, subtract, multiply, divide
+    """imports functions from the file calculator_1.py"""
+    from calculator_1 import add, su, mul, div
 
     a = 10
     b = 5
 
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, subtract(a, b)))
-    print("{} * {} = {}".format(a, b, multiply(a, b)))
-    print("{} / {} = {}".format(a, b, divide(a, b)))
-
+    print("{} - {} = {}".format(a, b, subt(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
