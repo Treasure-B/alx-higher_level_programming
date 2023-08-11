5958430648086b285240b826c6cb3950b479598c
Treasure-B <thabiso.magagula@gmail.com>
2023-08-11 14:18:12 -0700

    1-calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,12 +2,12 @@
 
 if __name__ == "__main__":
     """imports functions from the file calculator_1.py"""
-    from calculator_1 import add, su, mul, div
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
 
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, subt(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
