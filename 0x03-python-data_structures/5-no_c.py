8f767012878f555f153f7f82c4cd0baebb65c38f
Treasure-B <thabiso.magagula@gmail.com>
2023-08-14 09:43:39 -0700

    No Cc

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,6 +5,6 @@ def no_c(my_string):
     def no_c(my_string):
         new_string = ""
         for char in my_string:
-            if char.lower() != 'c' and char.upper() != 'C':
+            if char.lower() != 'c':
                 new_string += char
                 return new_string
