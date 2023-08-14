216a3d5b58671bd6ce3bb9dc4b97b6cd6875b2ce
Treasure-B <thabiso.magagula@gmail.com>
2023-08-14 09:49:54 -0700

    No Cc

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,8 @@
 
 def no_c(my_string):
     """Remove all characters c and C from a string."""
-    def no_c(my_string):
-        new_string = ""
-        for char in my_string:
-            if char.lower() != 'c':
-                new_string += char
-                return new_string
+    new_string = ""
+    for char in my_string:
+        if char.lower() != 'c' and char.upper() != 'C':
+            new_string += char
+            return new_string
