6d63b5c2d1f1648745bfb461bce5acc77b81dc1c
Treasure-B <thabiso.magagula@gmail.com>
2023-08-15 11:03:47 -0700

    add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,7 +4,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
     """ If tuple_a or tuple_b has less than 2 integers, append"""
     if len(tuple_a) < 2:
         tuple_a += (0,)
+
     if len(tuple_b) < 2:
         tuple_b += (0,)
 
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    tuple_a = tuple_a[:2]
+    tuple_b = tuple_b[:2]
+
+    total_1 = tuple_a[0] + tuple_b[0]
+    total_2 = tuple_a[1] + tuple_b[1]
+
+    return (total_1, total_2)
