128a72595dfbf7af9f45d8107586cb7aa85d2d2e
Treasure-B <thabiso.magagula@gmail.com>
2023-08-15 10:27:28 -0700

    print_matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,12 @@
 
 def print_matrix_integer(matrix=[[]]):
     """ prints a matrix of integers"""
+
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" " if num != row[-1] else "")
-            print()
+        for num in range(len(row)):
+            if num != len(row) - 1:
+                print("{:d} ".format(row[num]), end="")
+            else:
+                print("{:d}".format(row[num]), end="")
+        print()
+
