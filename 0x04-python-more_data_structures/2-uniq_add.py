f9121db50b21c99c4bf94f521f8976c1f1ed59ef
Treasure-B <thabiso.magagula@gmail.com>
2023-08-16 10:41:09 -0700

    uniq add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,8 +3,8 @@
 def uniq_add(my_list=[]):
     """adds all unique integers in a list"""
     uniq_set = set(my_list)
-    sum = 0
+    result = 0
 
     for element in uniq_set:
-        sum += element
-        return (sum)
+        result += element
+        return (result)
