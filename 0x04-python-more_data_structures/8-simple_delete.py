326907b864b5b8f3c5bbbd06a67e8915c093dc2c
Treasure-B <thabiso.magagula@gmail.com>
2023-08-16 15:18:25 -0700

    multiply_by_2

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,6 +2,5 @@
 
 def simple_delete(a_dictionary, key=""):
     """deletes a key in a dictionary."""
-    if key in a_dictionary:
-        del a_dictionary[key]
-        return a_dictionary
+    a_dictionary.pop(key, None)
+    return a_dictionary
