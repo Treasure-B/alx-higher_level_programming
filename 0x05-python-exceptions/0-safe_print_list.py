4907952ec5ccde6de053237c542a63b63a79ab81
Treasure-B <thabiso.magagula@gmail.com>
2023-08-28 14:28:44 -0700

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[j]), end=" ")
             num += 1
         except IndexError:
-            continue
+            break
     print("")
     return (num)
