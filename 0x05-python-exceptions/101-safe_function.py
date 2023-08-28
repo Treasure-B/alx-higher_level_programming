95de196328ec2e6698ab764dcbf8071ba3f1dd3a
Treasure-B <thabiso.magagula@gmail.com>
2023-08-28 15:06:04 -0700

    101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    """executes a function safely."""
+    try:
+        result = fct(*args)
+        return (result)
+    except Exception as e:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
