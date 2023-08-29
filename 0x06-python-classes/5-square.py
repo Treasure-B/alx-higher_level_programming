370a4d04235497c6ec82e2ed856dda2520befb1c
Treasure-B <thabiso.magagula@gmail.com>
2023-08-29 12:53:19 -0700

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """
+    A class representing a square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a square with the given size.
+
+        Args:
+            size (int, optional): The length of each side of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Get the size of the square.
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the square.
+
+        Args:
+            value (int): The new length of each side of the square.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate the area of the square
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """
+        Print a square pattern of '#' characters.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
