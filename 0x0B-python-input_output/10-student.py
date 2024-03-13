25a04785154ff3403033c4cfe150129f9808db8a
Treasure-B <thabiso.magagula@gmail.com>
2024-03-13 10:01:59 -0700

    ai

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,13 +3,7 @@
 
 
 class Student:
-    """Represent a student.
-
-    Attributes:
-        first_name (str): The first name of the student.
-        last_name (str): The last name of the student.
-        age (int): The age of the student.
-    """
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
@@ -31,12 +25,8 @@ class Student:
 
         Args:
             attrs (list): (Optional) The attributes to represent.
-
-        Returns:
-            dict: A dictionary representation of the Student.
         """
-        if isinstance(attrs, list) and all(isinstance(ele, str) for ele in attrs):
-            return {
-                k: getattr(self, k) for k in attrs if hasattr(self, k)
-            }
-        return self.__dict___
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
